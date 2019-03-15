14c02f3a247296972ab1b4ec6fec37901a04d011
nc <yeongeechong@gmail.com>
2019-03-15 12:36:03 +0800

    Added before_action authenticate for all events actions except index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index
     @events = Event.all
